575bbc1df466a4f9a7f1bb2e999ab924ea86672c
SaherHeib <45973462+SaherHeib@users.noreply.github.com>
2021-09-19 16:11:38 +0300

    Netflix2 app

---
+++ netflix2-app/src/pages/Home.js
@@ -0,0 +1,49 @@
+import "./Home.css";
+import Row from "../components/Row";
+import requests from "../API/requests";
+import Banner from "../components/Banner";
+import Nav from "../components/Nav";
+const Home = () => {
+  return (
+    <div className="home">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row
+        title="Horror Movies"
+        id="HM"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row
+        title="Romance  Movies"
+        id="RM"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row
+        title="Documentaries"
+        id="DM"
+        fetchUrl={requests.fetchDocumentaries}
+      />
+    </div>
+  );
+};
+
+export default Home;
