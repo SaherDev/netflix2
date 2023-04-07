2f477d73f0f91fe271fe9512d13f75260d3906a7
Saher <saherdev4@gmail.com>
2023-04-07 19:50:23 +0300

    Removing the User avatar

--- netflix2-app/src/components/Nav.js
+++ netflix2-app/src/components/Nav.js
@@ -25,11 +25,6 @@ const Nav= ()=> {
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
                 className="nav__logo"
             />
-            <img
-                alt="User logged"
-                src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-                className="nav__avatar"
-            />
         </nav>
     )
 }
